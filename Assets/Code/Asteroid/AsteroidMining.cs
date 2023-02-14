04b0235accc8a5850a0f60a412e64055b0788a31
CandyShyy <kupciol.chmiel@gmail.com>
2023-02-15 00:07:47 +0100

    minimapa działa :3

--- Assets/Code/Asteroid/AsteroidMining.cs
+++ Assets/Code/Asteroid/AsteroidMining.cs
@@ -7,12 +7,19 @@ public class AsteroidMining : MonoBehaviour
     public int minMinerals = 2;
     public int maxMinerals = 10;
 
+    public Sprite littleMinedSprite;
+    public Sprite halfMinedSprite;
+    public Sprite fullyMinedSprite;
+
     private int currentMinerals;
+    private SpriteRenderer parentSpriteRenderer; // reference to the parent's sprite renderer
 
     private void Start()
     {
         maxMinerals = Random.Range(minMinerals, maxMinerals);
         currentMinerals = maxMinerals;
+
+        parentSpriteRenderer = transform.parent.GetComponent<SpriteRenderer>(); // get the parent's sprite renderer component
     }
 
     public int Mine(int amount) // modified to return the number of minerals mined
@@ -20,14 +27,25 @@ public class AsteroidMining : MonoBehaviour
         currentMinerals -= amount;
         if (currentMinerals <= 0)
         {
-            // The asteroid is depleted, so you can destroy it and its parent
-        Transform parentTransform = transform.parent;
-        Destroy(gameObject);
-        if (parentTransform != null)
-        {
-            Destroy(parentTransform.gameObject);
+            // The asteroid is depleted, so you can destroy it and update the parent's sprite
+            if (parentSpriteRenderer != null)
+            {
+                parentSpriteRenderer.sprite = fullyMinedSprite;
+                Destroy(gameObject);
+            }
         }
-    }
-        return amount; // return the number of minerals mined
+        else if (currentMinerals <= maxMinerals / 2)
+        {
+            // Half of the minerals are mined, change sprite
+            parentSpriteRenderer.sprite = halfMinedSprite;
+        }
+        else if (currentMinerals <= maxMinerals -1)
+        {
+            // Little of the minerals are mined, change sprite
+            parentSpriteRenderer.sprite = littleMinedSprite;
+        }
+            return amount; // return the number of minerals mined
     }
 }
+
+
