ffba679d0c7dc5efd79d50209c85512002999578
CandyShyy <kupciol.chmiel@gmail.com>
2023-02-17 18:54:30 +0100

    TechTree upgrade

--- Assets/Code/AstroMiner/ACMinerals.cs
+++ Assets/Code/AstroMiner/ACMinerals.cs
@@ -7,15 +7,12 @@ public class ACMinerals : MonoBehaviour
     [Header("All Minerals")]
     public int Minerals;
 
-    private string playerTag = "Player";
-    private string mineralZone = "mineralZone";
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         // Check if the collider is tagged as a MineralsZone and the player is in contact with it
-        if (other.CompareTag(mineralZone) && other.GetComponent<Collider2D>().IsTouching(GameObject.FindGameObjectWithTag(playerTag).GetComponent<Collider2D>()))
+        if (other.CompareTag("mineralZone") && other.GetComponent<Collider2D>().IsTouching(GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>()))
         {
-            ACMining playerMining = GameObject.FindGameObjectWithTag(playerTag).GetComponent<ACMining>();
+            ACMining playerMining = GameObject.FindGameObjectWithTag("Player").GetComponent<ACMining>();
 
             // Add the player's mined minerals to the ACMinerals script
             Minerals += playerMining.totalMineralsHarvested;
--- Assets/Code/UI/UpgradeTrigger.cs
+++ Assets/Code/UI/UpgradeTrigger.cs
@@ -9,25 +9,22 @@ public class UpgradeTrigger : MonoBehaviour
     public ACMining acMining; 
 
     private Rigidbody2D acRigidbody;
-    private string playerTag = "Player";
-    private string mineralZone = "mineralZone";
+    private Collider2D playerCollider;
 
     private void Start()
     {
         upgradeUI.SetActive(false);
-        acRigidbody = acController.GetComponent<Rigidbody2D>();       
+        acRigidbody = acController.GetComponent<Rigidbody2D>();
+        playerCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>(); 
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         // Check if the collider is tagged as a MineralsZone and the player is in contact with it
-        if (other.CompareTag(mineralZone) && other.GetComponent<Collider2D>().IsTouching(GameObject.FindGameObjectWithTag(playerTag).GetComponent<Collider2D>()))
+    if (other.CompareTag("mineralZone") && other.GetComponent<Collider2D>().IsTouching(GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>()))
         {
-            // Show the shop UI
             upgradeUI.SetActive(true);
-            // Disable the ACController script
             acController.enabled = false;
-            // Freeze the ACController rigidbody
             acRigidbody.constraints = RigidbodyConstraints2D.FreezeAll;
         }
     }
